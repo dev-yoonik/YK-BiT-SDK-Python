28ac48195fadd543a2931a71025d72f7ee78d81f
João Ferreira <jppdpf@outlook.pt>
2021-10-13 11:10:00 +0100

    Update run_bit_sample.py

--- sample/run_bit_sample.py
+++ sample/run_bit_sample.py
@@ -16,33 +16,31 @@ def base64_to_file(filename: str, data: str):
         fh.write(base64.decodebytes(data.encode('utf-8')))
 
 
-def main():
+if __name__ == "__main__":
+    # Status
     bit_availability = status()
     print(f"BiT Availability: {bit_availability} \n")
 
-    if bit_availability == BiTStatus.NotAvailable:
-        return
+    if bit_availability == BiTStatus.Available:
+        # Capture
+        captured = capture(capture_timeout=10)
+        print(f"Capture: \n"
+              f"\t Status: {captured.capture_status} \n"
+              # f"\t Image: {captured.image} \n"
+              )
+        base64_to_file("captured", captured.image)
 
-    captured = capture(capture_timeout=10)
-    print(f"Capture: \n"
-          f"\t Status: {captured.capture_status} \n"
-          # f"\t Image: {captured.image} \n"
-          )
-    base64_to_file("captured",  captured.image)
+        # Verify
+        verified = verify(reference_image=captured.image, capture_time_out=10, anti_spoofing=True)
+        print(f"Verify: \n"
+              f"\t Matching Score: {verified.matching_score} \n"
+              f"\t Status: {verified.verify_status} \n"
+              # f"\t Verified Image: {verified.verified_image} \n"
+              )
+        base64_to_file("verified", verified.verified_image)
 
-    verified = verify(reference_image=captured.image, capture_time_out=10, anti_spoofing=True)
-    print(f"Verify: \n"
-          f"\t Matching Score: {verified.matching_score} \n"
-          f"\t Status: {verified.verify_status} \n"
-          # f"\t Verified Image: {verified.verified_image} \n"
-          )
-    base64_to_file("verified", verified.verified_image)
-
-    verified_images = verify_images(probe_image=captured.image, reference_image=verified.verified_image)
-    print(f"Verify Images: \n"
-          f"\t Matching Score: {verified_images.matching_score} \n"
-          f"\t Status: {verified_images.verify_images_status} \n")
-
-
-if __name__ == "__main__":
-    main()
+        # Verify Images
+        verified_images = verify_images(probe_image=captured.image, reference_image=verified.verified_image)
+        print(f"Verify Images: \n"
+              f"\t Matching Score: {verified_images.matching_score} \n"
+              f"\t Status: {verified_images.verify_images_status} \n")
